22bd7dfa0263943f4200759062e7de0cd9063794
Qirui Huang <qirui.huang@arm.com>
2026-07-31 14:58:11 +0100

    Add extension device object and array type
    Signed-off-by: Qirui Huang <qirui.huang@arm.com>

---
+++ platform/extension_device.go
@@ -0,0 +1,332 @@
+package platform
+
+import (
+	"fmt"
+
+	"github.com/veraison/psatoken"
+)
+
+type ExtensionDevice struct {
+	// HashAlgorithm identifies the hash algorithm used for the digest fields
+	HashAlgorithm            *string `cbor:"1,keyasint,omitempty" json:"hash-algorithm,omitempty"`
+	DeviceMeasurementsDigest *[]byte `cbor:"2,keyasint" json:"device-measurements-digest"`
+	CertificateChainDigest   *[]byte `cbor:"3,keyasint" json:"certificate-chain-digest"`
+
+	// UsesIDE indicates whether the platform device uses Integrity & Data Encryption.
+	UsesIDE *bool `cbor:"4,keyasint" json:"uses-ide"`
+
+	// Protocol identifies the protocol used to communicate with the device.
+	Protocol *Protocol `cbor:"5,keyasint" json:"protocol"`
+
+	// VCADigest is required when Protocol is one of the protocols-support-vca, otherwise no VCADigest field is expected
+	VCADigest *[]byte `cbor:"6,keyasint,omitempty" json:"vca-digest,omitempty"`
+
+	// DeviceType identifies the type of platform extension device.
+	DeviceType *DeviceType `cbor:"7,keyasint" json:"device-type"`
+
+	// EncryptionType is required for cxl-type-3, otherwise no EncryptionType field is expected
+	EncryptionType *EncryptionType `cbor:"8,keyasint,omitempty" json:"encryption-type,omitempty"`
+}
+
+type Protocol string
+
+// Protocols supporting VCA
+const (
+	ProtocolSPDM120 Protocol = "spdm-1.2.0"
+	ProtocolSPDM121 Protocol = "spdm-1.2.1"
+	ProtocolSPDM122 Protocol = "spdm-1.2.2"
+	ProtocolSPDM123 Protocol = "spdm-1.2.3"
+	ProtocolSPDM130 Protocol = "spdm-1.3.0"
+	ProtocolSPDM131 Protocol = "spdm-1.3.1"
+	ProtocolSPDM132 Protocol = "spdm-1.3.2"
+	ProtocolSPDM140 Protocol = "spdm-1.4.0"
+)
+
+// Protocols supporting VCA require a VCA digest.
+func (p Protocol) RequiresVCADigest() bool {
+	switch p {
+	case ProtocolSPDM120,
+		ProtocolSPDM121,
+		ProtocolSPDM122,
+		ProtocolSPDM123,
+		ProtocolSPDM130,
+		ProtocolSPDM131,
+		ProtocolSPDM132,
+		ProtocolSPDM140:
+		return true
+	default:
+		return false
+	}
+}
+
+func ValidateProtocolAndVCADigest(p *Protocol, v *[]byte) error {
+	if p == nil {
+		return fmt.Errorf("protocol is required")
+	}
+
+	if p.RequiresVCADigest() {
+		if v == nil {
+			return fmt.Errorf("protocol %s requires a VCA digest", *p)
+		}
+		err := psatoken.ValidatePSAHashType(*v)
+		if err != nil {
+			return fmt.Errorf("invalid VCA digest: %w", err)
+		}
+	}
+
+	if !p.RequiresVCADigest() && v != nil {
+		return fmt.Errorf("VCA digest is not expected for protocol %s", *p)
+	}
+
+	return nil
+}
+
+// DeviceType identifies the type of platform extension device.
+type DeviceType string
+
+const (
+	DeviceTypeCXLType3 DeviceType = "cxl-type-3"
+)
+
+func (t DeviceType) RequiresEncryptionType() bool {
+	switch t {
+	case DeviceTypeCXLType3:
+		return true
+	default:
+		return false
+	}
+}
+
+type EncryptionType int32
+
+const (
+	HostSideEncryption EncryptionType = iota
+	TargetSideEncryption
+	NoEncryption
+)
+
+func (d ExtensionDevice) Validate() error {
+	if err := psatoken.FilterError(d.GetHashAlgorithm()); err != nil {
+		return fmt.Errorf("hash algorithm: %w", err)
+	}
+
+	if err := psatoken.FilterError(d.GetDeviceMeasurementsDigest()); err != nil {
+		return fmt.Errorf("device measurements digest: %w", err)
+	}
+
+	if err := psatoken.FilterError(d.GetCertificateChainDigest()); err != nil {
+		return fmt.Errorf("certificate chain digest: %w", err)
+	}
+
+	if err := psatoken.FilterError(d.GetUsesIDE()); err != nil {
+		return fmt.Errorf("usesIDE: %w", err)
+	}
+
+	if err := psatoken.FilterError(d.GetVCADigest()); err != nil {
+		return fmt.Errorf("VCA digest: %w", err)
+	}
+
+	if err := psatoken.FilterError(d.GetProtocol()); err != nil {
+		return fmt.Errorf("protocol: %w", err)
+	}
+
+	if err := psatoken.FilterError(d.GetEncryptionType()); err != nil {
+		return fmt.Errorf("encryption type: %w", err)
+	}
+
+	if err := psatoken.FilterError(d.GetDeviceType()); err != nil {
+		return fmt.Errorf("device type: %w", err)
+	}
+	return nil
+}
+
+func ValidateDeviceTypeAndEncryption(d *DeviceType, e *EncryptionType) error {
+	if d == nil {
+		return fmt.Errorf("device type is required")
+	}
+
+	if d.RequiresEncryptionType() {
+		if e == nil {
+			return fmt.Errorf("device type %s requires an encryption type", *d)
+		}
+		if *e != HostSideEncryption && *e != TargetSideEncryption && *e != NoEncryption {
+			return fmt.Errorf("invalid encryption type %s", *d)
+		}
+	}
+
+	if !d.RequiresEncryptionType() && e != nil {
+		return fmt.Errorf("encryption type is not expected for device type %s", *d)
+	}
+
+	return nil
+}
+
+func (d ExtensionDevice) GetHashAlgorithm() (string, error) {
+	if d.HashAlgorithm == nil {
+		return "", psatoken.ErrMandatoryFieldMissing
+	}
+
+	return *d.HashAlgorithm, nil
+}
+
+func (d ExtensionDevice) GetDeviceMeasurementsDigest() ([]byte, error) {
+	if d.DeviceMeasurementsDigest == nil {
+		return nil, psatoken.ErrMandatoryFieldMissing
+	}
+
+	if err := psatoken.ValidatePSAHashType(*d.DeviceMeasurementsDigest); err != nil {
+		return nil, err
+	}
+
+	return *d.DeviceMeasurementsDigest, nil
+}
+
+func (d ExtensionDevice) GetCertificateChainDigest() ([]byte, error) {
+	if d.CertificateChainDigest == nil {
+		return nil, psatoken.ErrMandatoryFieldMissing
+	}
+
+	if err := psatoken.ValidatePSAHashType(*d.CertificateChainDigest); err != nil {
+		return nil, err
+	}
+
+	return *d.CertificateChainDigest, nil
+}
+
+func (d ExtensionDevice) GetUsesIDE() (bool, error) {
+	if d.UsesIDE == nil {
+		return false, psatoken.ErrMandatoryFieldMissing
+	}
+
+	return *d.UsesIDE, nil
+}
+
+func (d ExtensionDevice) GetProtocol() (Protocol, error) {
+	err := ValidateProtocolAndVCADigest(d.Protocol, d.VCADigest)
+	if err != nil {
+		return "", err
+	}
+
+	return *d.Protocol, nil
+}
+
+func (d ExtensionDevice) GetVCADigest() ([]byte, error) {
+	err := ValidateProtocolAndVCADigest(d.Protocol, d.VCADigest)
+	if err != nil {
+		return nil, err
+	}
+
+	if !d.Protocol.RequiresVCADigest() {
+		return nil, fmt.Errorf("%w: VCA digest not expected for protocol %s", psatoken.ErrFieldNotInProfile, *d.Protocol)
+	}
+
+	return *d.VCADigest, nil
+}
+
+func (d ExtensionDevice) GetDeviceType() (DeviceType, error) {
+	err := ValidateDeviceTypeAndEncryption(d.DeviceType, d.EncryptionType)
+	if err != nil {
+		return "", err
+	}
+
+	return *d.DeviceType, nil
+}
+
+func (d ExtensionDevice) GetEncryptionType() (EncryptionType, error) {
+	err := ValidateDeviceTypeAndEncryption(d.DeviceType, d.EncryptionType)
+	if err != nil {
+		return 0, err
+	}
+
+	if !d.DeviceType.RequiresEncryptionType() {
+		return 0, fmt.Errorf("%w: encryption type not expected for device type %s", psatoken.ErrFieldNotInProfile, *d.DeviceType)
+	}
+
+	return *d.EncryptionType, nil
+}
+
+func (d *ExtensionDevice) SetHashAlgorithm(v string) error {
+	d.HashAlgorithm = &v
+	return nil
+}
+
+func (d *ExtensionDevice) SetDeviceMeasurementsDigest(v []byte) error {
+	if err := psatoken.ValidatePSAHashType(v); err != nil {
+		return err
+	}
+	d.DeviceMeasurementsDigest = &v
+	return nil
+}
+
+func (d *ExtensionDevice) SetCertificateChainDigest(v []byte) error {
+	if err := psatoken.ValidatePSAHashType(v); err != nil {
+		return err
+	}
+	d.CertificateChainDigest = &v
+	return nil
+}
+
+func (d *ExtensionDevice) SetUsesIDE(v bool) error {
+	d.UsesIDE = &v
+	return nil
+}
+
+// Protocol can only be set once. If it is already set, return an error.
+// This is to keep the validation logic for protocol and VCA digest simple.
+func (d *ExtensionDevice) SetProtocol(p Protocol) error {
+	if d.Protocol != nil {
+		return fmt.Errorf("protocol can only be set once and is already set to %s", *d.Protocol)
+	}
+	d.Protocol = &p
+
+	return nil
+}
+
+func (d *ExtensionDevice) SetVCADigest(h []byte) error {
+	if d.Protocol == nil {
+		return fmt.Errorf("protocol must be set before setting VCA digest")
+	}
+
+	if !d.Protocol.RequiresVCADigest() {
+		return fmt.Errorf("%w: VCA digest is not expected for protocol %s", psatoken.ErrFieldNotInProfile, *d.Protocol)
+	}
+
+	err := ValidateProtocolAndVCADigest(d.Protocol, &h)
+	if err != nil {
+		return err
+	}
+
+	d.VCADigest = &h
+
+	return nil
+}
+
+// Device type can only be set once. If it is already set, return an error.
+// This is to keep the validation logic for device type and encryption type simple.
+func (d *ExtensionDevice) SetDeviceType(t DeviceType) error {
+	if d.DeviceType != nil {
+		return fmt.Errorf("device type can only be set once and is already set to %s", *d.DeviceType)
+	}
+
+	d.DeviceType = &t
+
+	return nil
+}
+
+func (d *ExtensionDevice) SetEncryptionType(t EncryptionType) error {
+	if d.DeviceType == nil {
+		return fmt.Errorf("device type must be set before setting encryption type")
+	}
+
+	if !d.DeviceType.RequiresEncryptionType() {
+		return fmt.Errorf("%w: encryption type is not expected for device type %s", psatoken.ErrFieldNotInProfile, *d.DeviceType)
+	}
+
+	if t != HostSideEncryption && t != TargetSideEncryption && t != NoEncryption {
+		return fmt.Errorf("invalid encryption type: %d", t)
+	}
+
+	d.EncryptionType = &t
+
+	return nil
+}
---
+++ platform/extension_devices.go
@@ -0,0 +1,47 @@
+package platform
+
+import (
+	"fmt"
+
+	"github.com/veraison/psatoken"
+)
+
+type ExtensionDevices []*ExtensionDevice
+
+func (o ExtensionDevices) Validate() error {
+	if len(o) == 0 {
+		return psatoken.ErrOptionalClaimMissing
+	}
+
+	return validateExtensionDevices(o)
+}
+
+func (o ExtensionDevices) Copy() (ExtensionDevices, error) {
+	err := validateExtensionDevices(o)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := make(ExtensionDevices, len(o))
+	copy(ret, o)
+
+	return ret, nil
+}
+
+func (o ExtensionDevices) IsEmpty() bool {
+	return len(o) == 0
+}
+
+func validateExtensionDevices(vals ExtensionDevices) error {
+	for i, k := range vals {
+		if k == nil {
+			return fmt.Errorf("failed at index %d: %s", i, "nil key in ExtensionDevices")
+		}
+
+		if err := k.Validate(); err != nil {
+			return fmt.Errorf("failed at index %d: %w", i, err)
+		}
+	}
+
+	return nil
+}
